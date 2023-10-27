434f08e6e6a2a3c23fceea32947ec403d4021394
myrg15 <merishield@gmail.com>
2023-10-27 02:29:38 +0200

    feat: create file db index config json

---
+++ index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { AppDataSource } from "./db";
+
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 4000
+
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Database connected');
+    
+    app.listen(PORT, () => {
+      console.log(`Server running ${PORT}`);
+    })
+  })
+  .catch(error => {
+    console.log(error)
+  })
